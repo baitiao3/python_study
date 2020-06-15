59a8fb523495f1f93786194a473119f69fbbe083
baitiao3 <18066408059@163.com>
2020-06-15 21:38:10 +0800

    第一次提交

---
+++ attend_school/day2/dome_1.py
@@ -0,0 +1,9 @@
+# diyige zhushi
+
+#if True:
+#	print("TRUE")
+#else:
+#	print("false")
+
+#import keyword
+#print(keyword.kwlist)
---
+++ attend_school/day3/dome_1.py
@@ -0,0 +1,62 @@
+#!D:\python
+
+#第一个注释
+#第二个注释
+
+'''
+第三个注释
+第四个注释
+'''
+
+"""
+第五个注释
+第六个注释
+"""
+
+# print("输出字符串")
+ 
+print("holle world!")
+
+# \ 多行语句
+
+total = 1 + \
+		2 + \
+		3
+print(total)
+
+#
+list = [1+1,'dome']
+print(list)#[2,'dome']
+
+
+number1 = 1.2 # float
+
+number2 = 1 # int
+
+print(number1+number2)
+
+number3 = 1.333E+2 # float
+
+print(number3)
+
+number4 = 2 + 1j
+
+print(number4)
+
+str = "baitiao"
+
+print(str)
+print(str[0:2])
+print(str[0])
+print(str[1:-1])
+print(str[2:])
+print(str*2)
+print(str+"233")
+
+print("this a enter \n yes")
+print(r"this a enter \n yes")
+
+if True:
+	print("True")
+else:
+	print("False")
